cd890816ff8e7ae1df85181180404d7e7f718a4a
boubelpi <113375173+boubelpi@users.noreply.github.com>
2024-07-12 13:38:04 +0200

    Parser for levels added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,39 @@ const App = () => {
     {
       title: 'Task 1',
       fileName: 'task1.py',
+      "bugs": {
+            "another_bad_name": "name",
+            "uppercase": "uppercase"
+      },
+    "code": [
+        "# Level 2",
+        {
+            "error": "uppercase",
+            "initial": `def transform(text, uppercase):
+    if uppercase:
+        return text.upper()
+    else:
+        return text.lower()
+            `,
+            "fixed": `
+            def uppercase(text):
+    return text.upper()
+
+def lowercase(text):
+    return text.lower()`
+        },
+        "print(\"Hello\")",
+        {
+            "error": "uppercase",
+            "initial": "print(transform(another_bad_name, True))",
+            "fixed": "print(uppercase(another_bad_name))"
+        },
+        "print(\"1213212\")"
+    ]
+    }/*,
+    {
+      title: 'Task 1',
+      fileName: 'task1.py',
       code: `
 # Welcome to the Clean Code Game! Your task is to rename the badly named variables to more meaningful names.
 # Click on the variable names to rename them. When all variables are renamed, you can proceed to the next task.
@@ -171,12 +204,159 @@ class ConfusingNames:
         a1: 'num1',
         a2: 'num2'
       }
-    }
+    },
+    {
+      title: 'Task 11',
+      fileName: 'task11.py',
+      code: `
+      from datetime import datetime
+genyyyymmddhhmmss = datetime.strptime('04/27/95 07:14:22', '%m/%d/%y %H:%M:%S')
+`,
+      variables: {
+        genyyyymmddhhmmss: 'generation_datetime'
+      }
+    },
+    {
+      title: 'Task 12',
+      fileName: 'task12.py',
+      code: `
+      def calculate_sum(a,b):
+      return a +b
+`,
+      variables: {
+        b:' b'
+      }
+    },
+    {
+      title: 'Task 13',
+      fileName: 'task13.py',
+      code: `
+      for i in range(5):
+print(i)
+`,
+      variables: {
+        print: '      print'
+      }
+    },
+    {
+      title: 'Task 14',
+      fileName: 'task14.py',
+      code: `
+      client_first_name = 'Bob'
+customer_last_name = 'Smith'
+`,
+      variables: {
+        customer_last_name: 'client_last_name'
+      }
+    },
+    {
+      title: 'Task 15',
+      fileName: 'task15.py',
+      code: `
+      class Person:
+    def __init__(self, person_first_name, person_last_name, person_age):
+        self.person_first_name = person_first_name
+        self.person_last_name = person_last_name
+        self.person_age = person_age
+`,
+      variables: {
+        person_first_name: 'first_name',
+        person_last_name: 'last_name',
+        person_age: 'age'
+      }
+    },
+    {
+      title: 'Task 16',
+      fileName: 'task16.py',
+      code: `
+      def transform(text, uppercase):
+    if uppercase:
+        return text.upper()
+    else:
+        return text.lower()
+`,
+      variables: {
+        def: `
+         def uppercase(text):
+    return text.upper()
+
+def lowercase(text):
+    return text.lower()
+        `
+      }
+    },
+    {
+      title: 'Task 17',
+      fileName: 'task17.py',
+      code: `
+      pi = 3.14159
+r = 5.0
+print(pi * r**2)
+`,
+      variables: {
+        pi: 'PI'
+      }
+    },
+    {
+      title: 'Task 18',
+      fileName: 'task18.py',
+      code: `
+      def calculate_sum(a, __name__):
+      return a + __name__
+`,
+      variables: {
+        __name__: 'name'
+      }
+    },
+    {
+      title: 'Task 19',
+      fileName: 'task19.py',
+      code: `
+      class LongMethodNames:
+  def a(self, first_element_in_function):
+    return first_element_in_function + 1
+
+  def b(self, second_element_in_function):
+    return second_element_in_function - 1
+`,
+      variables: {
+        'a': 'increment',
+        'b': 'decrement',
+        'first_element_in_function': 'x',
+        'second_element_in_function': 'y'
+      }
+    },
+    {
+      title: 'Task 20',
+      fileName: 'task20.py',
+      code: `
+      def BadName(a, b):
+      return a + b
+`,
+      variables: {
+        'BadName': 'sum'
+      }
+    }*/
   ];
 
+  const [set, setOfFixedErrors] = useState(new Set())  
+  const [renamedVariables, setRenamedVariables] = useState(tasks[0].bugs);
+
+  function parseCode(code) {
+    const map = code.map(item => (typeof item === 'string' ? item.trim() : (set.has(item.error) ? item.fixed.trim() : item.initial.trim())))
+    let answer = ""
+    for (const item of map) {
+      answer += item + '\n'
+    }
+    for (const item of set) {
+      answer = answer.replace(item, renamedVariables[item])
+    }
+    return answer
+  }
+
   const [selectedTaskIndex, setSelectedTaskIndex] = useState(0);
-  const [code, setCode] = useState(tasks[0].code.trim());
-  const [renamedVariables, setRenamedVariables] = useState(tasks[0].variables);
+  const [code, setCode] = useState(parseCode(tasks[0].code));
+  const [bugs, allTheBugs] = useState(Object.keys(tasks[0].bugs))
   const [renamed, setRenamed] = useState({});
   const [completed, setCompleted] = useState(false);
   const [completedTasks, setCompletedTasks] = useState([]);
@@ -199,7 +379,7 @@ class ConfusingNames:
 
   const handleTaskSelect = (index) => {
     setSelectedTaskIndex(index);
-    setCode(tasks[index].code.trim());
+    setCode(tasks[index].code);
     setRenamedVariables(tasks[index].variables);
     setCompleted(false);
     setRenamed({});
@@ -209,16 +389,29 @@ class ConfusingNames:
   };
 
   const handleVariableClick = (oldName) => {
+    console.log(set)
     if (disabled) return;
 
-    if (oldName && renamedVariables[oldName]) {
+    if (oldName && renamedVariables[oldName] && !set.has(oldName)) {
       const newName = renamedVariables[oldName];
+      if (oldName === newName) {
+       const newSet = set.add(oldName)
+       setOfFixedErrors(newSet)
+       const newCode = parseCode(tasks[0].code)
+       setCode(newCode);
+       setRenamed((prev) => ({ ...prev, [oldName]: true }));
+      }
+      else {
       const newCode = code.replace(new RegExp(`\\b${oldName}\\b`, 'g'), newName);
+      const newSet = set.add(oldName)
+      setOfFixedErrors(newSet)
+      console.log(oldName)
       setCode(newCode);
       setRenamed((prev) => ({ ...prev, [oldName]: true }));
-
-      const allRenamed = Object.keys(renamedVariables).every((variable) => newCode.includes(renamedVariables[variable]));
-      if (allRenamed) {
+      }
+      console.log(set)
+      console.log(Object.keys(bugs).size)
+      if (set.size === 2) { //should be changed to the number of bugs
         setCompleted(true);
         setCompletedTasks((prev) => [...prev, selectedTaskIndex]);
         setTerminalMessage('Success: All variables have been renamed!');

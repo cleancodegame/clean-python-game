094a1429ad88a6e8536dc709eeea2200f5dbc147
Klestid Poloska <klestidp@gmail.com>
2024-08-26 00:08:46 +0200

    Levels 10-18

---
+++ public/12.py
@@ -0,0 +1,11 @@
+## Task 12
+## task12.py
+## replace myclass
+class myclass:
+    def __init__(self):
+        self.name = "example"
+## with
+class MyClass:
+    def __init__(self):
+        self.name = "example"
+## end
---
+++ public/13.py
@@ -0,0 +1,11 @@
+## Task 13
+## task13.py
+## replace this_is_one_thing and\n    that_is_another_thing
+if (this_is_one_thing and
+    that_is_another_thing):
+    do_something()
+## with
+if (this_is_one_thing
+        and that_is_another_thing):
+    do_something()
+

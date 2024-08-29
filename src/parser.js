00f566e80c3f1085b913f30faf1408df789ff1d7
Pavel Egorov <pe.xoposhiy@gmail.com>
2024-08-29 18:41:13 +0200

    parser moved to separate file

--- src/parser.js
+++ src/parser.js
@@ -1,97 +1,227 @@
-export let tasks = []
-function parserFromPython(dataInArray) {
-  let finalCode = []
-  let bugsFound = {}
-  let prevWord = ""
-  let num = 0
-  let levelTitle = ""
-  let levelFilename = ""
-  let curInitialCode = ""
-  let curFixedCode = ""
-  let curMistaken = ""
-  let initialCode = false
-  let blockOfIncorrectLines = false
-  for (const lineIndex in dataInArray) {
-    const item = dataInArray[lineIndex]
-    let words = item.split(" ")
-    if (words[0] !== "##" && !blockOfIncorrectLines) {
-      finalCode.push(item)
-      continue
+function parsePythonLines(lines) {
+  let currentLine = 0;
+  function parseUntilLine(endLine) {
+    let code = "";
+    while (currentLine < lines.length && lines[currentLine] !== endLine) {
+      code += lines[currentLine] + "\n";
+      currentLine++;
     }
-    if (words[0] !== "##") {
-      if (initialCode === true) {
-        curInitialCode += item + "\n"
+    if (currentLine < lines.length)
+      currentLine++; // skip the endLine
+    return code;
+  }
+
+  function parseReplace() {
+    // ## replace-on event-id
+    // ...
+    // ## with
+    // ...
+    // ## end
+    // ## replace event-id [substring]
+    // ...
+    // ## with
+    // ...
+    // ## end
+    let instructionName = lines[currentLine].split(" ")[1];
+    let replaceArgs = parseInstruction("## " + instructionName, 2);
+    if (instructionName === "replace-on"){
+      if (replaceArgs.length != 1)
+        throw Error("Expected 1 argument for replace-on, got " + replaceArgs.length);
+    }
+    let initialCode = parseUntilLine("## with");
+    let finalCode = parseUntilLine("## end");
+    return {
+      actionType: instructionName,
+      eventId: replaceArgs[0],
+      substring: replaceArgs[1],
+      code: initialCode,
+      replacementCode: finalCode,
+    };
+  }
+
+  function parseRemove() {
+    let initialCode = "";
+    let eventId = "";
+    let type = "";
+    while (currentLine < lines.length) {
+      if (lines[currentLine] === "## end") {
+        break;
+      } else if (lines[currentLine][0] + lines[currentLine][1] !== "##") {
+        initialCode += lines[currentLine];
+      } else {
+        let allWords = lines[currentLine].split(" ");
+        type = allWords[1];
+        eventId = allWords[2];
       }
-      else {
-        curFixedCode += item + "\n"
+      currentLine += 1;
+    }
+    currentLine += 1;
+    return {
+      blockType: type,
+      eventId: eventId,
+      substring: null, //?
+      code: initialCode,
+      replacementCode: "",
+    };
+  }
+  function parseAdd() {
+    let finalCode = "";
+    let eventId = "";
+    let type = "";
+    while (currentLine < lines.length) {
+      if (lines[currentLine] === "## end") {
+        break;
+      } else if (lines[currentLine][0] + lines[currentLine][1] !== "##") {
+        finalCode += lines[currentLine];
+      } else {
+        let allWords = lines[currentLine].split(" ");
+        type = allWords[1];
+        eventId = allWords[2];
       }
-      continue
+      currentLine += 1;
     }
-    if (lineIndex === 0) {
-      levelTitle = words[1] + " " + words[2]
+    currentLine += 1;
+    return {
+      actionType: type,
+      eventId: eventId,
+      substring: null, //?
+      code: "",
+      replacementCode: finalCode,
+    };
+  }
+
+  function parseInstruction(instruction, argsCount) {
+    const line = lines[currentLine];
+    if (!line.startsWith(instruction))
+      throw Error(
+        "Expected instruction " + instruction + " but got " + lines[currentLine]
+      );
+    currentLine++;
+    let argsLine = line.slice(instruction.length + 1);
+    let args = [];
+    for (let i = 0; i < argsCount-1; i++) {
+      let spaceIndex = argsLine.indexOf(" ");
+      if (spaceIndex === -1)
+        break;
+      args.push(argsLine.slice(0, spaceIndex));
+      argsLine = argsLine.slice(spaceIndex + 1);
     }
-    else if (lineIndex === 1) {
-      levelFilename = words[1]
-    }
-    else if (words[1] === "error") {
-      curMistaken = words[2]
-      initialCode = true
-      blockOfIncorrectLines = true
-    }
-    else if (words[1] === "fix") {
-      initialCode = false
-    }
-    else if (words[1] === "end") {
-      if (typeof bugsFound[curMistaken] == "undefined") {
-        num = num + 1
+    args.push(argsLine);
+    return args;
+  }
+
+  function parseExplain() {
+    // ## explain event-id explanation
+    let args = parseInstruction("## explain", 2);
+    return {
+      actionType: "explain",
+      eventId: args[0],
+      explanation: args[1]
+    };
+  }
+
+  function parseReplaceInline() {
+    // ## replace-inline event-id code
+    // ## with replacement
+    let replaceArgs = parseInstruction("## replace-inline", 2);
+    let replacementCode = parseInstruction("## with", 1);
+    return {
+      actionType: "replace-inline",
+      eventId: replaceArgs[0],
+      substring: null,
+      code: replaceArgs[1],
+      replacementCode: replacementCode,
+    };
+  }
+  function parseBlock() {
+    let curLine = lines[currentLine];
+    if (
+      curLine.startsWith("## replace ") ||
+      curLine.startsWith("## replace-on ")
+    ) {
+      return parseReplace();
+    } else if (
+      curLine.startsWith("## remove ") ||
+      curLine.startsWith("## remove-on ")
+    ) {
+      return parseRemove();
+    } else if (curLine.startsWith("## replace-inline ")) {
+      return parseReplaceInline();
+    } else if (
+      curLine.startsWith("## add ") ||
+      curLine.startsWith("## add-on ")
+    ) {
+      return parseAdd();
+    } else if (curLine.startsWith("## explain ")) {
+      return parseExplain();
+    } else {
+      let code = "";
+      while (
+        currentLine < lines.length &&
+        !lines[currentLine].startsWith("## ")
+      ) {
+        code += lines[currentLine] + "\n";
+        currentLine += 1;
       }
-      bugsFound[curMistaken] = curMistaken
-      finalCode.push({
-        "error": curMistaken,
-        "initial": curInitialCode,
-        "fixed": curFixedCode
-      })
-      curMistaken = ""
-      initialCode = false
-      blockOfIncorrectLines = false
-      curInitialCode = ""
-      curFixedCode = ""
-    }
-    else if (words[1] === "mistake") {
-      prevWord = words[words.length - 1]
-    }
-    else if (words[1] === "correct") {
-      bugsFound[prevWord] = words[words.length - 1]
-      num = num + 1
+      return {
+        actionType: "text",
+        code: code,
+      };
     }
   }
-  const item = {
-    title: levelTitle,
-    fileName: levelFilename,
-    'bugs': bugsFound,
-    'number': num,
-    'code': finalCode
+
+  function parseHeaderLine() {
+    let spaceIndex = lines[currentLine].indexOf(" ");
+    let line = lines[currentLine];
+    currentLine += 1;
+    return line.slice(spaceIndex + 1);
   }
-  if (typeof item !== "undefined") {
-  return item
-}
-}
-async function parserFromFile(fileName) {
-  let response = await fetch(fileName)
-  let data = await response.text()
-  return data.split(RegExp("\\r?\\n"))
+
+  let title = parseHeaderLine();
+  let filename = parseHeaderLine();
+  let blocks = [];
+  while (currentLine < lines.length) {
+    blocks.push(parseBlock());
+  }
+  return {
+    title: title,
+    filename: filename,
+    blocks: blocks,
+  };
 }
 
-async function makeArray(fileName) {
-  let anotherTasks = []
-  let queryAnswer = await parserFromFile(fileName)
-  anotherTasks.push(parserFromPython(queryAnswer))
-  return anotherTasks
+async function readFileLines(fileName) {
+  let response = await fetch(fileName);
+  let data = await response.text();
+  return data.split(RegExp("\\r?\\n"));
 }
 
-async function makeFinalArray(fileNames) {
+async function loadTask(fileName) {
+  let lines = await readFileLines(fileName);
+  return parsePythonLines(lines);
+}
+
+function formatTaks(task) {
+  let result = task.title + "\n" + task.filename + "\n";
+  for (const block of task.blocks) {
+    if (block.actionType === "text")
+      result += block.code
+    else if (block.actionType === "replace" 
+      || block.actionType === "replace-on" 
+      || block.actionType === "remove"
+      || block.actionType === "remove-on"
+      || block.actionType === "text"
+    ) {
+      result += block.code;
+    }
+    return result;
+  }
+}
+
+export async function loadTasksTo(fileNames, tasks) {
   for (const curFile of fileNames) {
-  let task = await makeArray(curFile);
-  tasks.push(...task);
+    let task = await loadTask(curFile);
+    console.log(formatTaks(task));
+    tasks.push(task);
   }
 }

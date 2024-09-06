781ed7bee1cafad5734e433d181faf2d77d45831
Pavel Egorov <pe.xoposhiy@gmail.com>
2024-09-06 19:24:26 +0200

    Review results

--- src/codeTasks.js
+++ src/codeTasks.js
@@ -1,69 +1,78 @@
 function processWithReplaceInline(text, eventsHappened) {
-  let id = 0
-  let processedText = ""
-  while (id < text.length) {
-    let hasEvents = false
-    for (const block of task.blocks) {
-     if (block.actionType === "replace-inline" && id + block.code.length < text.length && block.eventId in eventsHappened) {
-      if (text.substring(id, id + block.code.length) === block.code) {
-        processedText += block.replacementCode
-        id += block.code.length
-        hasEvents = true
-        break
+  let i = 0;
+  let processedText = "";
+
+  let inlineReplaceBlocks = task.blocks.filter(
+    b => b.actionType === "replace-inline" 
+    && b.eventId in eventsHappened
+  ); 
+
+  while (i < text.length) {
+    let hasReplacement = false;
+    for (const inlineReplaceBlock of inlineReplaceBlocks) {
+      const pattern = inlineReplaceBlock.code;
+      const replacement = inlineReplaceBlock.replacementCode;
+      if (i + pattern.length < text.length) {
+        if (text.substring(i, i + patter.length) === pattern) {
+          processedText += replacement;
+          i += pattern.length;
+          hasReplacement = true;
+          break;
+        }
       }
-     }
     }
-    if (!hasEvents) {
-      processedText += text[id]
-      id += 1
+    if (!hasReplacement) {
+      processedText += text[i];
+      i += 1;
     }
   }
-  return processedText
+  return processedText;
 }
 
 export function formatTask(task, eventsHappened) {
-  let result = ""
+  let result = "";
   for (const block of task.blocks) {
     if (block.actionType === "text")
-      result += processWithReplaceInline(block.code, eventsHappened)
-    else if (block.actionType === "replace" 
-      || block.actionType === "replace-on" 
-      || block.actionType === "remove"
-      || block.actionType === "remove-on"
-    )
-    {
-      if (task.eventId in eventsHappened) {
-          result += block.replacementCode;
+      result += processWithReplaceInline(block.code, eventsHappened);
+    else if (
+      block.actionType === "replace" ||
+      block.actionType === "replace-on" ||
+      block.actionType === "remove" ||
+      block.actionType === "remove-on" ||
+      block.actionType === "add-on"
+    ) {
+      if (block.eventId in eventsHappened) {
+        result += processWithReplaceInline(block.replacementCode, eventsHappened);
+      } else {
+        result += processWithReplaceInline(block.code, eventsHappened);
+      }
     }
-    else {
-      result += block.code;
+    else if (block.actionType === "replace-inline" || block.actionType === "explain") {
+      // do nothing
     }
+    else 
+      throw new Error("Unknown block type: " + block.actionType);
   }
-  }
-  //DONE
-  //TODO: implement replace-inline (create a region for all substrings)
   return result;
 }
 
-export function getEventRegions(task, eventsHappened){
+export function getEventRegions(task, eventsHappened) {
   let curLine = 1;
   let eventRegions = [];
   for (const block of task.blocks) {
-    if (block.actionType === "text")
-      curLine += block.code.split("\n").length;
+    if (block.actionType === "text") curLine += block.code.split("\n").length;
     else if (block.actionType === "replace") {
       if (block.eventId in eventsHappened) {
         curLine += block.replacementCode.split("\n").length;
-      }
-      else{
-        if (block.substring === null){
+      } else {
+        if (block.substring === null) {
           let blockLines = block.code.split("\n");
           let eventRegion = {
             startLine: curLine,
             startColumn: 1,
             endLine: curLine + blockLines.length - 1,
-            endColumn: blockLines[blockLines.length-1].length + 1,
-            eventId: block.eventId
+            endColumn: blockLines[blockLines.length - 1].length + 1,
+            eventId: block.eventId,
           };
           eventRegions.push(eventRegion);
         }
